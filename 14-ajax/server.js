0308b6fd8212db92326a25b71db5b07b7d3b696b
Lúcio Flavio da Conceição Matias <lucioflavio.cmatias90@hotmail.com>
2019-11-28 08:25:56 -0300

    Fetch Api

--- 14-ajax/server.js
+++ 14-ajax/server.js
@@ -51,5 +51,12 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 // Startar o servidor, digitando: "npm start"
 app.listen(8080, () => console.log('Executando...'))
